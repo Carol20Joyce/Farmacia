a2609474cad9ab133bd67f6fcbd4fb3df0e38bdd
Joyce Caroline Rodrigues <carolinerodriguesjoyce@gmail.com>
2023-04-24 15:29:44 -0300

    farmacia

---
+++ src/main/java/com/generation/farmacia/controller/RemedioController.java
@@ -0,0 +1,82 @@
+package com.generation.farmacia.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.generation.farmacia.model.Remedio;
+import com.generation.farmacia.repository.CategoriaRepository;
+import com.generation.farmacia.repository.RemedioRepository;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/remedios")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class RemedioController {
+
+	@Autowired
+	private RemedioRepository remedioRepository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@GetMapping
+	public ResponseEntity<List<Remedio>> getAll() {
+		return ResponseEntity.ok(remedioRepository.findAll());
+
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ResponseEntity<Remedio>> getById(@PathVariable Long id) {
+		return remedioRepository.findById(id).map(resposta -> ResponseEntity.ok(resposta))
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+
+	}
+
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Remedio>> getByNome(@PathVariable String nome) {
+		return ResponseEntity.ok(remedioRepository.findAllByNomeContainingIgnoreCase(nome));
+
+	}
+
+	@PostMapping
+	public ResponseEntity<Remedio> postRemedio(@Valid @RequestBody Remedio remedio){
+		return categoriaRepository.findById(remedio.getCategoria().getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.CREATED).body(remedioRepository.save(remedio)))
+				.orElse(ResponseEntity.badRequest().build());
+
+	}
+
+	@PutMapping
+	public void put(@PathVariable Long id, @RequestBody Remedio remedio) {
+		Optional<Remedio> post = remedioRepository.findById(id);
+		if (post.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		ResponseEntity.status(HttpStatus.OK).body(remedioRepository.save(remedio));
+
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deleteById(@PathVariable Long id) {
+		return remedioRepository.findById(id).map(resposta -> {
+			remedioRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		}).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+
+}
---
+++ src/main/java/com/generation/farmacia/model/Remedio.java
@@ -0,0 +1,108 @@
+package com.generation.farmacia.model;
+
+import java.math.BigDecimal;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_remedios") 
+public class Remedio {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O Atributo nome é obrigatório")
+	@Size(min = 5, max = 100, message = "O atributo nome deve ter no mínimo 05 e no máximo 100 caracteres")
+	private String nome;
+	
+	@NotBlank(message = "O Atributo descrição é obrigatório")
+	@Size(min = 10, max = 1000, message = "O atributo descrição deve ter no mínimo 10 e no máximo 1000 caracteres")
+	private String descricao;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("remedio")
+	private Categoria categoria;
+	
+	private int quantidade;
+	
+	private String laboratorio;
+	
+	private BigDecimal preco;
+	
+	private String foto;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public String getLaboratorio() {
+		return laboratorio;
+	}
+
+	public void setLaboratorio(String laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+}
